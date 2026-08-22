79c0019ad178ae13666af39b164d1e3fe0e963f6
Lysander <deridot1@gmail.com>
2026-08-22 13:33:45 +0800

    Made buttons bigger by making text size 30, I also made it so that when you press submit it will automatically remove what you entered in the text bar so that users have feedback that the text was put in the file.

--- myFirstGUI.py
+++ myFirstGUI.py
@@ -15,7 +15,7 @@ def submit():
         NAME_OF_TEXT_FILE, "a", encoding="UTF-8"
     ) as file_obj:  # Writes contents to a text file.
         file_obj.write(f"{response}\n")
-
+    entry.delete(0, END)
 
 def delete():  # Deletes text from the text bar.
     entry.delete(0, END)
@@ -48,19 +48,19 @@ window.title("Enter word to be put inside a text file")
 entry = Entry(window, font=("Arial", 50))
 entry.pack()
 
-submit_button = Button(window, text="submit", command=submit)
+submit_button = Button(window, font=("Arial", 30), text="submit", command=submit)
 submit_button.pack()
 
-delete_button = Button(window, text="delete", command=delete)
+delete_button = Button(window, font=("Arial", 30), text="delete", command=delete)
 delete_button.pack()
 
-show_contents_of_text_button = Button(window, text="show current contents of text", command=showContentsOfText)
+show_contents_of_text_button = Button(window, font=("Arial", 30), text="show current contents of text", command=showContentsOfText)
 show_contents_of_text_button.pack()
 
-delete_text_contents_button = Button(window, text="Delete current contents of text", command=deleteCurrentTextContents)
+delete_text_contents_button = Button(window, font=("Arial", 30), text="Delete current contents of text", command=deleteCurrentTextContents)
 delete_text_contents_button.pack()
 
-quit_button = Button(window, text="quit", command=quit)
+quit_button = Button(window, font=("Arial", 30), text="quit", command=quit)
 quit_button.pack()
 
 window.mainloop()
